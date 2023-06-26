38e01779070c5cfe0039d59f7e8f93be432e010a
sajidCode <muhammadsajidkhan616@gmail.com>
2023-06-26 20:00:28 +0500

    changed about/intro text

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,30 +3,31 @@ import { Header } from "./reusable-components/Header";
 import "./home.css";
 import { Link } from "react-router-dom";
 export const Home = ({ animationPage }) => {
-  console.log(animationPage);
-  return (
-    <div className="home">
-      <div className="home-content container">
-        <div className="home-text">
-          <h1 className="home-title">Introduction</h1>
-          <p className="home-para1">
-            The scientific reasons for going to Mars can be summarised by the
-            search for life, understanding the surface and the planet's
-            evolution, and preparing for future human exploration.
-          </p>
-          <p className="home-para2">
-            Understanding whether life existed elsewhere in the Universe beyond
-            Earth is a fundamental question of humankind.
-          </p>
-        </div>
-        <Link to="/BookATrip" className="btn">
-          Book A Trip
-        </Link>
-      </div>
+    console.log(animationPage);
+    return (
+        <div className="home">
+            <div className="home-content container">
+                <div className="home-text">
+                    <h1 className="home-title">Introduction</h1>
+                    <p className="home-para1">
+                        Welcome to our futuristic website! Discover a world
+                        beyond imagination as we offer ticket booking services
+                        for the ultimate adventure—journey to Mars. Step into a
+                        realm of limitless possibilities and embark on a cosmic
+                        odyssey.
+                    </p>
+                    <p className="home-para2">
+                        Buckle up and let your dreams soar among the stars.
+                    </p>
+                </div>
+                <Link to="/BookATrip" className="btn">
+                    Book A Trip
+                </Link>
+            </div>
 
-      <div className="circles">
-        <div className="circle"></div>
-      </div>
-    </div>
-  );
+            <div className="circles">
+                <div className="circle"></div>
+            </div>
+        </div>
+    );
 };
